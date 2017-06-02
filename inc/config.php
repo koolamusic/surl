777fa0a7d1742bc2d4a346032872840348ce27ca
tecmie <admin@tecmie.com>
2017-06-02 01:44:34 +0100

    Update with File changes in local

--- inc/config.php
+++ inc/config.php
@@ -1,15 +1,15 @@
 <?php
 $host="localhost"; // Host name - localhost or view in hosting cpanel what they give you
-$username="addurl-359f52"; // Mysql username
-$password="84j5uJfxc6kG"; // Mysql password
-$db_name="addurl-359f52"; // DataBase name
+$username="root"; // Mysql username
+$password="MYPASSWORD123"; // Mysql password
+$db_name="addm"; // DataBase name
 
 //define ("SITE_URL","http://add.url.ph"); 
-define ("SITE_URL","http://rockradio.host/eco"); // your website name here 
+define ("SITE_URL","http://localhost/add"); // your website name here 
 define ("IP",$_SERVER['REMOTE_ADDR']); 
 define ("LANG","en");
-define ("SITENAME","Add Site Info");  // your logo is here
-define ("SLOGAN","Submit All Sites"); 
+define ("SITENAME","Ponzi Database");  // your logo is here
+define ("SLOGAN","TRACK EVERY PONZI"); 
 
 // Connect to server and select database
 mysql_connect("$host", "$username", "$password")or die("cannot connect");
